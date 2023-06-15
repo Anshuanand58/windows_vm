76f3c33a7d76b1152660a611398d25241dbee747
Anshu Anand <109796743+Anshuanand58@users.noreply.github.com>
2023-06-15 11:42:16 +0530

    Update and rename wsidrdcapp01.tf to test-vm.tf

--- test-vm.tf
+++ test-vm.tf
@@ -1,26 +1,26 @@
-module "wsidrdcapp01" {
-  source              = "git::https://xkmjl6xcuanap5betn3m43lagh5y6llpn6lq5bqfpaxep7cynvca@dev.azure.com/IRL-INFRA-DEVOPS/IRL-MODULES/_git/IRL-MODULES//azurerm_windows_virtual_machine"
+module "test-vm" {
+  source              = "location of your main code"
   location            = "South india"
-  resource_group_name = "rsg-si-inf-dr-net-01"
-  subnet_name         = "snt-si-inf-dr-dc-01"
-  vnet_name           = "vnt-si-inf-dr-hub-01"
-  vnet_rg_name        = "rsg-si-inf-dr-net-01"
+  resource_group_name = "test-rg"
+  subnet_name         = "default"
+  vnet_name           = "test-vnet"
+  vnet_rg_name        = "test-rg"
   create_public_ip    = false
   public_ip_name      = ""
   pip_add_allocation  = "Static"
   private_ip_address  = "10.16.41.69"
   vm_config = {
     "vm1" = {
-      name           = "wsidrdcapp001"
+      name           = "test-vm"
       size           = "Standard_D4s_v3"
       admin_username = "azureadmin"
       admin_password = "Pa55w.rd@123"
-      computer_name  = "wsidrdcapp001"
+      computer_name  = "test-vm"
     }
   }
   caching_type     = "ReadWrite"
   disk_type        = "Standard_LRS"
-  os_disk_name     = "wsidrdcapp001"
+  os_disk_name     = "test-vm"
   os_disk_size_gb  = "128"
   os_publisher     = "MicrosoftWindowsServer"
   os_offer         = "WindowsServer"
@@ -29,7 +29,7 @@ module "wsidrdcapp01" {
   create_data_disk = true
   data_disk_config = {
     "disk1" = {
-      name                 = "wsidrdcapp001-datadisk01"
+      name                 = "test-vm-datadisk01"
       storage_account_type = "Standard_LRS"
       disk_size_gb         = "128"
       lun                  = "1"
@@ -37,11 +37,9 @@ module "wsidrdcapp01" {
       vmkey                = "vm1"
     }
   }
-  nic_name = "nic-si-inf-dr-dc-01"
+  nic_name = "test-nic"
   tags = {
-    application        = "domain controller"
-    environment        = "dr"
-    IRL-business-owner = "Rajdeep Dutta"
-    tower              = "Infra"
+    application        = "test"
+
   }
 }
